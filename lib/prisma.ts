980586c71eb00951e8fa50d91d01d3f1e4af1c7a
Victor Homic <dev@3x071c.com>
2022-02-27 15:23:12 +0100

    ci: less noise, more juice (#125)
    🗿 Group up minor renovate changes into a single weekly PR (closes #107)
    💰 Add a Next.js build cache (closes #105)
    🙌 Increase the CI timeout (closes #106)
    🖥 Default to assuming a production deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,7 +7,7 @@ declare global {
 
 // https://pris.ly/d/help/next-js-best-practices
 export default global.prisma ||
-	(process.env.NODE_ENV === "production" && new PrismaClient()) ||
+	(process.env.NODE_ENV !== "development" && new PrismaClient()) ||
 	(global.prisma = new PrismaClient({
 		errorFormat: "pretty",
 		log: ["query", "info", "warn", "error"],
