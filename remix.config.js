4aa7159e233fda56476a5155f9dc5fb2f83802f9
Victor Homic <dev@3x071c.com>
2022-03-11 16:45:53 +0100

    fix: remix app server (#146)
    🌐 Use the Remix app server (not Vercel lol)

--- remix.config.js
+++ remix.config.js
@@ -9,7 +9,5 @@ module.exports = {
 	devServerPort: 8002,
 	ignoredRouteFiles: [".*"],
 	publicPath: "/build/",
-	server: process.env.NODE_ENV !== "development" ? "./server.js" : undefined,
 	serverBuildPath: ".remix/server.js",
-	serverBuildTarget: "vercel",
 };
