046604f8b37c2530f3b5802085291d9772c31c55
3x071c <dev@3x071c.com>
2022-03-11 16:40:20 +0100

    chore: fix

--- server.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import * as build from "@remix-run/dev/server-build";
-import { createRequestHandler } from "@remix-run/vercel";
-
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
